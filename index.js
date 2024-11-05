3b665de2a0a184a2b7937387eafa64327223f3bf
sugarhouse333 <tara.francis24@gmail.com>
2024-11-05 18:58:55 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function patChallenges() {
+    if (document.getElementById("challenges").src == "pix/challenges1.png"){
+        document.getElementById("challenges").src = "pix/challenges.png";
+    } 
+    else {
+        document.getElementById("challenges").src = "pix/challenges1.png";
+    }
+}
