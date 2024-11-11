2bb624fbdf0c419f2915069059a66e11ff6ae5d4
sugarhouse333 <tara.francis24@gmail.com>
2024-11-11 12:13:24 +1100

    right side nav link images and javascript hover

--- public/index.js
+++ public/index.js
@@ -1,8 +1,29 @@
-function patChallenges() {
-    if (document.getElementById("challenges").src == "pix/challenges1.png"){
-        document.getElementById("challenges").src = "pix/challenges.png";
-    } 
-    else {
-        document.getElementById("challenges").src = "pix/challenges1.png";
+function dreamHover(x, image) {
+    if (x==1) {
+        image.src = 'pix/dreamshover.gif'
+    }
+
+    if (x==2) {
+        image.src = 'pix/dreams.gif'
+    }
+}
+
+function galleryHover(x, image) {
+    if (x==1) {
+        image.src = 'pix/galleryhover.gif'
+    }
+
+    if (x==2) {
+        image.src = 'pix/gallery.gif'
+    }
+}
+
+function linksHover(x, image) {
+    if (x==1) {
+        image.src = 'pix/linkshover.gif'
+    }
+
+    if (x==2) {
+        image.src = 'pix/links.gif'
     }
 }
