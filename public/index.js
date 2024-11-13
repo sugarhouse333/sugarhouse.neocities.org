81a215b2c9c5928eb1f1f6a9566003a4bf8b2f03
sugarhouse333 <tara.francis24@gmail.com>
2024-11-13 11:39:28 +1100

    massive layout changes and button navigation system using js

--- public/index.js
+++ public/index.js
@@ -37,3 +37,46 @@ function challengesHover(x, image) {
         image.src = 'pix/bigchallenges-window.png'
     }
 }
+
+function togglePage(x) {
+    var home = document.getElementById("home")
+    var about = document.getElementById("about")
+    var blog = document.getElementById("blog")
+    var chat = document.getElementById("chat")
+    var guestbook = document.getElementById("guestbook")
+    if (x == 1) {
+        home.style.display = "block"
+        about.style.display = "none"
+        blog.style.display = "none"
+        chat.style.display = "none"
+        guestbook.style.display = "none"
+    }
+    if (x == 2) {
+        home.style.display = "none"
+        about.style.display = "block"
+        blog.style.display = "none"
+        chat.style.display = "none"
+        guestbook.style.display = "none"
+    }
+    if (x == 3) {
+        home.style.display = "none"
+        about.style.display = "none"
+        blog.style.display = "block"
+        chat.style.display = "none"
+        guestbook.style.display = "none"
+    }
+    if (x == 4) {
+        home.style.display = "none"
+        about.style.display = "none"
+        blog.style.display = "none"
+        chat.style.display = "block"
+        guestbook.style.display = "none"
+    }
+    if (x == 5) {
+        home.style.display = "none"
+        about.style.display = "none"
+        blog.style.display = "none"
+        chat.style.display = "none"
+        guestbook.style.display = "block"
+    }
+}
